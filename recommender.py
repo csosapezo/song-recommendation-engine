eb052807307f53a1cd9f8285b16f0efc7e8b5b63
Carlos Sosa <carlos.sosa@credigob.pe>
2023-11-30 13:00:41 +0000

    recommend by artist

--- recommender.py
+++ recommender.py
@@ -1,7 +1,9 @@
 """recommender functions"""
 
 import attrs
+import numpy as np
 import pandas as pd
+from scipy import stats
 
 
 @attrs.define
@@ -9,6 +11,7 @@ class Recommender:
     """recommender"""
     df: pd.DataFrame = pd.read_csv("song_dataset.csv")
     dist_df: pd.DataFrame = None
+    sum_dist_df: pd.DataFrame = None
 
     def get_song_distribution(self) -> pd.DataFrame:
         """get distribution DataFrame"""
@@ -16,6 +19,24 @@ class Recommender:
             "song",
             "user",
         ]).count()["play_count"].unstack(level=0).fillna(0)
+
+    def get_sum_song_distribution(self) -> pd.DataFrame:
+        """get distribution DataFrame (considering play count)"""
+        self.sum_dist_df = self.df.groupby([
+            "song",
+            "user",
+        ]).sum()["play_count"].unstack(level=0).fillna(0)
+
+    def get_songs_from_artist(self, song_id: str) -> np.ndarray:
+        """get all the songs from an artist in the database"""
+
+        artist_name: str
+        artist_name = self.df.loc[self.df.song == song_id].artist_name.iloc[0]
+
+        songs: pd.Series
+        songs = self.df.loc[self.df.artist_name == artist_name].song
+
+        return songs.unique()
 
     def recommend_from_song(
             self,
@@ -29,6 +50,27 @@ class Recommender:
         similar_users_df = similar_users_df.drop(song_list, axis=1)
 
         recommended_df: pd.Series = similar_users_df.sum()
-        recommended_df = recommended_df.loc[recommended_df > 0]
 
-        return recommended_df.sort_values(ascending=False).head()
+        return stats.zscore(recommended_df)
+
+    def recommend_from_artist(
+            self,
+            song_id: str,
+            song_list: list[str]
+            ) -> pd.Series:
+        """get a list of new songs the user has never listened"""
+
+        artist_songs: np.ndarray = self.get_songs_from_artist(song_id)
+
+        similar_users_df: pd.DataFrame
+        similar_users_df = self.sum_dist_df.loc[
+            (self.sum_dist_df[artist_songs] > 0).any(axis=1)
+        ]
+        similar_users_df = similar_users_df.multiply(
+            similar_users_df[song_id], axis="index"
+        )
+        similar_users_df = similar_users_df.drop(song_list, axis=1)
+
+        recommended_df: pd.Series = similar_users_df.sum()
+
+        return stats.zscore(recommended_df)
